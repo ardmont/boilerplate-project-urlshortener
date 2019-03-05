5b621f473ba15e01736f27a6b04185128864d548
Ardiles <ardiles.montenegro@gmail.com>
2019-03-05 12:43:25 -0300

    Adds dns module and uses const declarations.

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 'use strict'
 
-var express = require('express')
-var mongo = require('mongodb')
-var mongoose = require('mongoose')
+const express = require('express')
+const mongo = require('mongodb')
+const mongoose = require('mongoose')
+const dns = require('dns');
 
-var cors = require('cors')
+const cors = require('cors')
 
-var app = express()
+const app = express()
 
 // Basic Configuration
-var port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 /** this project needs a db !! **/
 // mongoose.connect(process.env.MONGOLAB_URI);
